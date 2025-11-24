40cb1d20895344d91197c0fea7e6c9d81e51e5f5
Velizar Stavrev <velizar.g.stavrev@gmail.com>
2025-11-24 23:23:25 +0200

    Adding Vitest coverage settings with istanbul.

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'html', 'lcov'],
+      include: ['src/**/*.ts'],
+      exclude: ['**/*.spec.ts'],
+    },
+  },
+});
